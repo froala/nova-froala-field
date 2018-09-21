1d6867cd8b9e15706e4b325c1bb20566560b70d3
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2018-09-21 19:21:57 +0300

    Initial commit 🚀

---
+++ resources/js/field.js
@@ -0,0 +1,5 @@
+Nova.booting((Vue, router) => {
+    Vue.component('index-nova-froala-field', require('./components/IndexField'));
+    Vue.component('detail-nova-froala-field', require('./components/DetailField'));
+    Vue.component('form-nova-froala-field', require('./components/FormField'));
+})
---
+++ src/NovaFroalaField.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Froala\NovaFroalaField;
+
+use Laravel\Nova\Fields\Field;
+
+class NovaFroalaField extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'nova-froala-field';
+}
