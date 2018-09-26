bb482fb9c189beef89bb32506e88370f2a4f557d
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2018-09-26 18:09:50 +0300

    Style fixes

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -2,10 +2,10 @@
     <default-field :field="field">
         <template slot="field">
             <input :id="field.name" type="text"
-                class="w-full form-control form-input form-input-bordered"
-                :class="errorClasses"
-                :placeholder="field.name"
-                v-model="value"
+                   class="w-full form-control form-input form-input-bordered"
+                   :class="errorClasses"
+                   :placeholder="field.name"
+                   v-model="value"
             />
 
             <p v-if="hasError" class="my-2 text-danger">
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
+import { FormField, HandlesValidationErrors } from 'laravel-nova';
 
 export default {
     mixins: [FormField, HandlesValidationErrors],
@@ -28,22 +28,22 @@ export default {
          * Set the initial, internal value for the field.
          */
         setInitialValue() {
-          this.value = this.field.value || ''
+            this.value = this.field.value || '';
         },
 
         /**
          * Fill the given FormData object with the field's internal value.
          */
         fill(formData) {
-          formData.append(this.field.attribute, this.value || '')
+            formData.append(this.field.attribute, this.value || '');
         },
 
         /**
          * Update the field's internal value.
          */
         handleChange(value) {
-          this.value = value
-        }
-    }
-}
+            this.value = value;
+        },
+    },
+};
 </script>
--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,5 @@
-Nova.booting((Vue, router) => {
+Nova.booting(Vue => {
     Vue.component('index-nova-froala-field', require('./components/IndexField'));
     Vue.component('detail-nova-froala-field', require('./components/DetailField'));
     Vue.component('form-nova-froala-field', require('./components/FormField'));
-})
+});
