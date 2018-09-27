e12d3a76c4c67cf0a2fa824f9c3d0696f9d1b1b3
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2018-09-27 23:26:34 +0300

    Integrated `vue-froala-wysiwyg`

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,4 +1,10 @@
+require('froala-editor/js/froala_editor.pkgd.min');
+
+import VueFroala from 'vue-froala-wysiwyg';
+
 Nova.booting(Vue => {
+    Vue.use(VueFroala);
+
     Vue.component('index-nova-froala-field', require('./components/IndexField'));
     Vue.component('detail-nova-froala-field', require('./components/DetailField'));
     Vue.component('form-nova-froala-field', require('./components/FormField'));
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,24 @@
-let mix = require('laravel-mix')
+let webpack = require('webpack');
+let mix = require('laravel-mix');
 
-mix.setPublicPath('dist')
+let distPath = 'dist';
+
+mix.setPublicPath(distPath)
    .js('resources/js/field.js', 'js')
    .sass('resources/sass/field.scss', 'css')
+   .styles([
+       'node_modules/froala-editor/css/froala_style.min.css',
+   ], distPath + '/css/froala_styles.min.css')
+   .copyDirectory('node_modules/font-awesome/fonts', distPath + '/fonts/font-awesome')
+   .webpackConfig({
+       resolve: {
+           symlinks: false
+       },
+       plugins: [
+           // Jquery loader plugin.
+           new webpack.ProvidePlugin({
+               $: "jquery",
+               jQuery: "jquery"
+           })
+       ]
+   });
