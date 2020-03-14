0ec964dd8f22eb14e90d09146b87a03f856d38fc
zippoxer <zippoxer@gmail.com>
2020-03-15 00:09:45 +0200

    include plugin assets inside the package

--- src/FroalaFieldServiceProvider.php
+++ src/FroalaFieldServiceProvider.php
@@ -24,16 +24,16 @@ class FroalaFieldServiceProvider extends ServiceProvider
         });
 
         Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-froala-field', __DIR__.'/../dist/js/field.js');
-            Nova::style('nova-froala-field', __DIR__.'/../dist/css/field.css');
+            Nova::script('nova-froala-field', __DIR__ . '/../dist/js/field.js');
+            Nova::style('nova-froala-field', __DIR__ . '/../dist/css/field.css');
 
             if ($this->app['config']->get('nova.froala-field.options.tuiEnable')) {
-                Nova::style('tui-editor', 'https://cdn.jsdelivr.net/npm/tui-image-editor@3.2.2/dist/tui-image-editor.css');
-                Nova::style('tui-color-picker', 'https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.css');
+                Nova::style('tui-editor', __DIR__ . '/../dist/plugins/tui-image-editor.css');
+                Nova::style('tui-color-picker', __DIR__ . '/../dist/plugins/tui-color-picker.css');
 
-                Nova::script('fabric', 'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.6.7/fabric.min.js');
-                Nova::script('tui-codesnippet', 'https://cdn.jsdelivr.net/npm/tui-code-snippet@1.4.0/dist/tui-code-snippet.min.js');
-                Nova::script('tui-image-editor', 'https://cdn.jsdelivr.net/npm/tui-image-editor@3.2.2/dist/tui-image-editor.min.js');
+                Nova::script('fabric', __DIR__ . '/../dist/plugins/fabric.min.js');
+                Nova::script('tui-codesnippet', __DIR__ . '/../dist/plugins/tui-code-snippet.min.js');
+                Nova::script('tui-image-editor', __DIR__ . '/../dist/plugins/tui-image-editor.min.js');
             }
 
             if (Str::startsWith(
@@ -45,22 +45,22 @@ class FroalaFieldServiceProvider extends ServiceProvider
         });
 
         $this->publishes([
-            __DIR__.'/../dist/vendor/nova/froala' => public_path('vendor/nova/froala'),
+            __DIR__ . '/../dist/vendor/nova/froala' => public_path('vendor/nova/froala'),
         ], 'nova-froala-field-plugins');
 
         $this->publishes([
-            __DIR__.'/../dist/css/froala_styles.min.css' => public_path('css/vendor/froala_styles.min.css'),
+            __DIR__ . '/../dist/css/froala_styles.min.css' => public_path('css/vendor/froala_styles.min.css'),
         ], 'froala-styles');
 
         $this->publishes([
-            __DIR__.'/../config/froala-field.php' => config_path('nova/froala-field.php'),
+            __DIR__ . '/../config/froala-field.php' => config_path('nova/froala-field.php'),
         ], 'config');
 
-        if (! class_exists('CreateFroalaAttachmentTables')) {
+        if (!class_exists('CreateFroalaAttachmentTables')) {
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__.'/../database/migrations/create_froala_attachment_tables.php.stub' => database_path('migrations/'.$timestamp.'_create_froala_attachment_tables.php'),
+                __DIR__ . '/../database/migrations/create_froala_attachment_tables.php.stub' => database_path('migrations/' . $timestamp . '_create_froala_attachment_tables.php'),
             ], 'migrations');
         }
     }
@@ -78,7 +78,7 @@ class FroalaFieldServiceProvider extends ServiceProvider
 
         Route::middleware(['nova'])
             ->prefix('nova-vendor/froala-field')
-            ->group(__DIR__.'/../routes/api.php');
+            ->group(__DIR__ . '/../routes/api.php');
     }
 
     /**
@@ -88,7 +88,7 @@ class FroalaFieldServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/froala-field.php', 'nova.froala-field');
+        $this->mergeConfigFrom(__DIR__ . '/../config/froala-field.php', 'nova.froala-field');
 
         if (config('nova.froala-field.attachments_driver') === 'trix') {
             $this->app->bind(TrixAttachmentController::class, FroalaToTrixAttachmentAdapterController::class);
