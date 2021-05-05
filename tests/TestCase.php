1a9344a1ef82a00da28552c4f4b37922b8d3f476
wimski <wimreckman@gmail.com>
2021-05-05 20:33:10 +0200

    Replace database config with php evn vars

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -53,23 +53,6 @@ abstract class TestCase extends OrchestraTestCase
     }
 
     /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        // Setup default database to use sqlite :memory:
-        $app['config']->set('database.default', 'testbench');
-        $app['config']->set('database.connections.testbench', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-    }
-
-    /**
      * Set up the database.
      *
      * @param \Illuminate\Foundation\Application $app
